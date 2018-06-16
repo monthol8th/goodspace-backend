b9521e643142768c97846b3fefa85da5c777228a
M.Charattrakool <m.charattrakool@gmail.com>
2018-06-17 00:07:44 +0700

    create children route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ import { Router } from 'express';
 import project from './project';
 import camp from './camp';
 import worker from './worker';
-
+import child from './child';
 
 const router = Router();
 
 router.use('/projects', project);
 router.use('/camps', camp);
 router.use('/workers', worker);
+router.use('/children', child);
 
 export default router;
