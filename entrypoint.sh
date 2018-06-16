1541b909262a0a39ed1391c183475366028ed0d5
M.Charattrakool <m.charattrakool@gmail.com>
2018-06-16 14:17:11 +0700

    setup db orm

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 npm i
+node_modules/.bin/sequelize db:create
+node_modules/.bin/sequelize db:migrate
 exec "$@"
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('postgres://postgres@db:5432/ysttr');
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
