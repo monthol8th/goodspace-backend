9f4dcaf5c066e4422abf84444aa91f5a1cc4c29b
M.Charattrakool <m.charattrakool@gmail.com>
2018-06-17 04:18:17 +0700

    camp can search with name

--- src/routes/worker.js
+++ src/routes/worker.js
@@ -12,13 +12,90 @@ import {
 const Sequelize = require('sequelize');
 const {
   Worker,
+  Camp,
 } = require('../db');
+
 const router = Router();
+
 
 router.get('/', async (_, res) => {
   try {
     const row = await Worker.findAll();
     respondResult(res)(row);
+  } catch (err) {
+    if (err instanceof Sequelize.ValidationError) {
+      respondBadReq(res)(err);
+    } else {
+      respondErrors(res)(err);
+    }
+  }
+});
+
+router.get('/search', async (req, res) => {
+  try {
+    const {
+      Op,
+    } = Sequelize;
+    const {
+      name,
+      id,
+      camp_name: campName,
+      p,
+    } = req.query;
+    if (id) {
+      const data = await Worker.findById(req.params.id);
+
+      respondResult(res)({
+        data: [data],
+        count: 1,
+      });
+    } else if (campName) {
+      const {
+        rows: data,
+        count,
+      } = await Worker.findAndCountAll({
+        include: [{
+          model: Camp,
+          required: true,
+          attributes: ['name'],
+          where: {
+            name: {
+              [Op.like]: `%${campName}%`,
+            },
+          },
+        }],
+        limit: 6,
+        offset: 6 * (p - 1 || 0),
+        order: [
+          ['createdAt', 'DESC'],
+        ],
+      });
+      respondResult(res)({
+        data,
+        count,
+      });
+    } else {
+      const {
+        rows: data,
+        count,
+      } = await Worker.findAndCountAll({
+        where: {
+          name: {
+            [Op.like]: `%${name}%`,
+          },
+        },
+        limit: 6,
+        offset: 6 * (p - 1 || 0),
+        order: [
+          ['createdAt', 'DESC'],
+        ],
+      });
+
+      respondResult(res)({
+        data,
+        count,
+      });
+    };
   } catch (err) {
     if (err instanceof Sequelize.ValidationError) {
       respondBadReq(res)(err);
