6d6939aa282731d7839259abb3145d113f1ccfde
M.Charattrakool <m.charattrakool@gmail.com>
2018-06-17 12:54:49 +0700

    add children when list and get worker

--- src/routes/worker.js
+++ src/routes/worker.js
@@ -1,4 +1,6 @@
-import { Router } from 'express';
+import {
+  Router
+} from 'express';
 
 import CommonRoute from '../utils/commonRoute';
 
@@ -9,19 +11,70 @@ import {
 } from '../utils/response';
 
 const Sequelize = require('sequelize');
-const { Worker, Camp } = require('../db');
+const {
+  Worker,
+  Camp,
+  Child,
+} = require('../db');
 
 const router = Router();
 
-router.get('/', CommonRoute.list(Worker));
-router.get('/:id', CommonRoute.get(Worker));
 router.put('/:id', CommonRoute.put(Worker));
 router.post('/', CommonRoute.post(Worker));
 
+router.get('/', async (req, res) => {
+  try {
+    const { p } = req.query;
+    const { rows: data, count } = await Worker.findAndCountAll({
+      include: [{
+        model: Child
+      }],
+      limit: 6,
+      offset: 6 * (p - 1 || 0),
+      order: [['createdAt', 'DESC']]
+    });
+
+    responseResult(res)({
+      data,
+      count
+    });
+  } catch (err) {
+    if (err instanceof Sequelize.ValidationError) {
+      responseBadReq(res)(err);
+    } else {
+      responseErrors(res)(err);
+    }
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const data = await Worker.findById(req.params.id);
+    if (!data) {
+      responseNotFound(res)();
+    }
+    const children = await data.getChildren();
+    const newData = {
+      ...data.toJSON(),
+      Children: children
+    }
+    responseResult(res)(newData);
+  } catch (err) {
+    responseErrors(res)(err);
+  }
+});
+
 router.get('/search', async (req, res) => {
   try {
-    const { Op } = Sequelize;
-    const { name, id, camp_name: campName, p } = req.query;
+    const {
+      Op
+    } = Sequelize;
+    const {
+      name,
+      id,
+      camp_name: campName,
+      p
+    } = req.query;
     if (id) {
       const data = await Worker.findById(req.params.id);
 
@@ -30,29 +83,35 @@ router.get('/search', async (req, res) => {
         count: 1
       });
     } else if (campName) {
-      const { rows: data, count } = await Worker.findAndCountAll({
-        include: [
-          {
-            model: Camp,
-            required: true,
-            attributes: ['name'],
-            where: {
-              name: {
-                [Op.like]: `%${campName}%`
-              }
+      const {
+        rows: data,
+        count
+      } = await Worker.findAndCountAll({
+        include: [{
+          model: Camp,
+          required: true,
+          attributes: ['name'],
+          where: {
+            name: {
+              [Op.like]: `%${campName}%`
             }
           }
-        ],
+        }],
         limit: 6,
         offset: 6 * (p - 1 || 0),
-        order: [['createdAt', 'DESC']]
+        order: [
+          ['createdAt', 'DESC']
+        ]
       });
       responseResult(res)({
         data,
         count
       });
     } else {
-      const { rows: data, count } = await Worker.findAndCountAll({
+      const {
+        rows: data,
+        count
+      } = await Worker.findAndCountAll({
         where: {
           name: {
             [Op.like]: `%${name}%`
@@ -60,7 +119,9 @@ router.get('/search', async (req, res) => {
         },
         limit: 6,
         offset: 6 * (p - 1 || 0),
-        order: [['createdAt', 'DESC']]
+        order: [
+          ['createdAt', 'DESC']
+        ]
       });
 
       responseResult(res)({
@@ -81,8 +142,7 @@ router.get('/stat/nationality', async (req, res) => {
   try {
     const result = await Worker.findAll({
       attributes: [
-        'nationality',
-        [Sequelize.fn('COUNT', 'nationality'), 'count']
+        'nationality', [Sequelize.fn('COUNT', 'nationality'), 'count']
       ],
       group: ['nationality']
     });
